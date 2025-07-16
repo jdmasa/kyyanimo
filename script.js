ac2e3ecf349acc7749fd5b6463b4471ef8e128b5
jdmasa <47989615+jdmasa@users.noreply.github.com>
2025-07-16 16:37:18 +0200

    Updated script.js

--- script.js
+++ script.js
@@ -330,7 +330,7 @@ class FieldGameTracker {
 // Register service worker for PWA functionality
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js')
+        navigator.serviceWorker.register('sw.js')
             .then((registration) => {
                 console.log('SW registered: ', registration);
             })
